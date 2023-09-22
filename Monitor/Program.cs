f3c4261c25342a142d9672185f9b379c59cfcc4d
varashus <rigodavid100@gmail.com>
2023-09-22 11:19:11 +0200

    Add project files.

---
+++ Monitor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Monitor
+{
+    class Program
+    {
+        
+        static double bruttoraktar(List<Vasar> monitorok)
+        {
+            double ossz = 0;
+
+            foreach (var x in monitorok)
+            {
+                ossz += x.brutto(x.ara, x.afa) * 15;
+                
+            }
+            return ossz;
+
+
+        }
+        static void Main(string[] args)
+        {
+            var monitorok = new List<Vasar>();
+            using var sr = new StreamReader
+                (
+                path: @"..\..\..\src\monitor.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+                sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                monitorok.Add(new Vasar(sr.ReadLine()));
+            }
+            foreach (var x in monitorok)
+            {
+                Console.WriteLine($"f1 gyárstó: {x.gyartoja}, típus: {x.tipusa} méret: {x.merete} nettó ár: {(x.ara/(x.afa/100))}");
+            }
+            
+            Console.WriteLine();
+            foreach (var x in monitorok)
+            {
+                Console.WriteLine(bruttoraktar());
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Monitor/Vasar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+    class Vasar
+    {
+        
+
+        public string gyartoja { get; set; }
+        public string tipusa { get; set; }
+        public double merete { get; set; }
+        public double ara { get; set; }
+        public string gamer { get; set; }
+        public double afa { get;}
+
+
+        public double brutto(double ara, double afa)
+        {
+            return (ara / afa) * 100;
+        }
+
+
+
+
+        public Vasar(string sor)
+        {
+            string[] r = sor.Split(';');
+            this.gyartoja = r[0];
+            this.tipusa = r[1];
+            this.merete = double.Parse(r[2]);
+            this.ara = double.Parse(r[3]);
+            if (r.Length == 4)
+            {
+                this.gamer = "gamer";
+            }
+            if (r.Length == 5)
+            {
+                this.gamer = null;
+            }
+            this.afa = 27;
+            
+
+        }
+       
+
+
+    }
+}
